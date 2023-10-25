070386b6135b7c1c6ed3a08e2c15e90792808cbe
Mikael Eriksson <m.eriksson@gmx.com>
2023-10-25 12:33:10 +0200

    Lad's videocreator microservice has been added.

--- VideoCreatorMicroService/src/main/java/com/lad666/videocreatorservice/Controller/VideoCreatorController.java
+++ VideoCreatorMicroService/src/main/java/com/lad666/videocreatorservice/Controller/VideoCreatorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/videocreators/api/edufy")
+@RequestMapping("api/edufy/videocreators")
 public class VideoCreatorController {
 
     @Autowired
