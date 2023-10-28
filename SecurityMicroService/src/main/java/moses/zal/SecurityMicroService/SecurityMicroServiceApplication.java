de199dcaf51096c60dfe83e45dc230c691429d02
Moussa Zalzala <moussa.zalzala@hotmail.com>
2023-10-28 06:33:44 +0200

    Added new microservice for security

---
+++ SecurityMicroService/src/main/java/moses/zal/SecurityMicroService/SecurityMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package moses.zal.SecurityMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMicroServiceApplication.class, args);
+	}
+
+}
